ad5eaf0400ed5c400358f03b4c8ba21e8e96eb9c
DASER DAVID <daserlinux@gmail.com>
2013-10-14 03:12:38 -0400

    testing

--- controllers/school.js
+++ controllers/school.js
@@ -7,16 +7,17 @@ function route(app) {
     var authentication = require('../lib/authentication/authentication');
     //app.post('/school/login', authentication.authenticate, function(req, res, next) {
     app.post('/school/login', function(req, res, next) {
-        var auth = require("../lib/authentication/accountProfile.js");
-        var dataSent = false;
-        auth.once('complete', function(data) {
-            if (dataSent) {
-                return;
-            }
-            dataSent = true;
-            res.json(data);
-        });
-        auth.bootStrap(req.body.identity, req.body.password);
+        res.send("yeso");
+//        var auth = require("../lib/authentication/accountProfile.js");
+//        var dataSent = false;
+//        auth.once('complete', function(data) {
+//            if (dataSent) {
+//                return;
+//            }
+//            dataSent = true;
+//            res.json(data);
+//        });
+//        auth.bootStrap(req.body.identity, req.body.password);
     });
 
 }
--- test/development/school/loginSpec.js
+++ test/development/school/loginSpec.js
@@ -5,8 +5,8 @@ var URL = 'http://cvbankcore.herokuapp.com';
 
 frisby.create('Ensure response has proper JSON types in specified keys')
 
-.get(URL + '/school/login')
-    .expectStatus(200)
+//.get(URL + '/school/login')
+//    .expectStatus(200)
     
 .post(URL + '/school/login', {
       identity: "daser",
