64246326af1231eef48551133883b32c330729e5
DASER DAVID <daserlinux@gmail.com>
2013-10-15 06:34:58 -0400

    account router

--- controllers/account.js
+++ controllers/account.js
@@ -3,7 +3,7 @@ function route(app) {
     var authentication = require('../lib/authentication/authentication');
 
     // account login processing
-    app.post('/account/login', authentication.authenticate, function (req, res, next) {
+    app.get('/account/login', authentication.authenticate, function (req, res, next) {
         res.send("Hello World");
     });
     
